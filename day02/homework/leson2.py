b30155bcf4779c8abf825cbed0eb78de0d7643fe
Elene-ha <eelenehack999@gmail.com>
2025-05-11 20:47:39 +0400

    second commit

---
+++ day02/homework/leson2.py
@@ -0,0 +1,18 @@
+# 1. ორი ცვლადის შექმნა და მათემატიკური ოპერეციები:
+
+number1 = 10  
+number2 = 5
+  
+print( number1 + number2)
+print( number1 - number2)
+print( number1 * number2)
+print( number1 / number2)
+print(number1 // number2)
+
+# 2. დაბადების წლის და მიმდინარე წლის გამოყენება ასაკის გამოთვლა:
+ 
+birth_year = 2000  #  დაბადების წელი
+current_year = 2025  # ახლანდელი წელი
+
+age = current_year - birth_year
+print(age)
