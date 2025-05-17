fbd034df36c822f55fa87994f3e1f0d000a525fa
Elene-ha <eelenehack999@gmail.com>
2025-05-17 14:52:24 +0400

    Third commit

---
+++ day01/Homework/homework01.py
@@ -0,0 +1,28 @@
+ # 1. მონაცემების შენახვა და მათი ჩვენება
+
+# ჩემი სახელი , ასაკი , სიმაღლე , წონა
+
+name = "Snake"
+age = 25
+height = 180 
+weight = 75  
+
+print(name)
+print(age)
+print(height)
+print(weight)
+
+# 2. მონაცემთა ტიპები და მაგალითები
+
+# მთელი რიცხვი (int)
+age = 25
+# სიმბოლო, ტექსტი (str)
+name = "Snake"
+# სემიანი რიცხვი (float)
+height = 180.5
+
+print(age)
+print(name)
+print(height)
+
+
--- day02/homework/leson02.py
+++ day02/homework/leson02.py
@@ -16,3 +16,17 @@ current_year = 2025  # ახლანდელი წელი
 
 age = current_year - birth_year
 print(age)
+
+# 3. 5 ცვლადი და საშუალო არითმეტიკული:
+
+number1 = 12
+number2 = 8
+number3 = 15
+number4 = 10
+number5 = 20
+
+total = number1 + number2 + number3 + number4 + number5
+average = total / 5
+
+print(total)
+print(average)
