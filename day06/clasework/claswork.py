21eef16320add4664456eb9a74c6cb5c7b4088d1
Elene-ha <eelenehack999@gmail.com>
2025-06-15 20:51:02 +0400

    Sixth commit

--- day06/clasework/claswork.py
+++ day06/clasework/claswork.py
@@ -1,5 +1,5 @@
 name = input("ჩემი  სახელი:ელენე ")
-surname = input(" ჩემი შეიყვანე  გვარი:ეფფგ ")
-age = input("ჩემი შეიყვანე  ასაკი:999 ")
+surname = input(" ჩემი გვარი:ეფფგ ")
+age = input("ჩემი  ასაკი:999 ")
 
 print("მე ვარ", name, surname, age, "წლის")
